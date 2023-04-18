ca933f41f8f11056f7f3be6464f127f8d4e0b4d2
Guillermo <zeduard89@gmail.com>
2023-04-18 20:35:49 -0300

    modelo bas

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Cards.Module.css'
+
+const Card = () => {
+
+//button check, maneja estado con true o false
+//button borrar elimina la card por id
+//card recibe un textarea con la info del form
+
+
+  return (
+    <div className='card'>Card</div>
+  )
+}
+
+export default Card
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Nav.Module.css'
+
+const Nav = () => {
+  return (
+    <div className="divNav"> Nav</div>
+  )
+}
+
+export default Nav
